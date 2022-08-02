0f3aafb8ae85523439998b82169477ba9e4cbea9
ehdbs0903 <ehdbs8747@naver.com>
2022-08-02 14:49:23 +0900

    Add mathematics/boj_1978.py

---
+++ mathematics/boj_1978.py
@@ -0,0 +1,15 @@
+import math
+
+n = int(input())
+nums = list(map(int, input().split()))
+
+cnt = 0
+for i in nums:
+    if i == 1:
+        cnt += 1
+    else:
+        for j in range(2, int(math.sqrt(i)) + 1):
+            if i % j == 0:
+                cnt += 1
+                break
+print(len(nums) - cnt)
