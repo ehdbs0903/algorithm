1a61cb35bbe1616a82267e4c89bbbb38865a5521
ehdbs0903 <ehdbs8747@naver.com>
2022-08-06 18:03:32 +0900

    Mathematics/boj_1026.py

---
+++ mathematics/boj_1026.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+b.sort()
+a.sort(reverse = True)
+s = 0
+for i in range(n):
+    s += a[i] * b[i]
+print(s)
