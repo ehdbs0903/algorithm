234d4369d7949d55bcf6667b233dc8d573c8b924
ehdbs0903 <ehdbs8747@naver.com>
2022-08-08 18:55:01 +0900

    boj_9020.py

---
+++ mathematics/boj_9020.py
@@ -0,0 +1,25 @@
+import math
+
+prime = []
+
+for i in range(2, 10000):
+    flag = 0
+    for j in range(2, int(math.sqrt(i) + 1)):
+        if i % j == 0:
+            flag = 1
+    if flag == 0:
+        prime.append(i)
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    left = n // 2
+    right = left
+    
+    while True:
+        if left in prime and right in prime:
+            print(left, right)
+            break
+        left -= 1
+        right += 1
