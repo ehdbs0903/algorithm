335a6c299a0a6e69f770aebbd4278626101a2477
ehdbs0903 <ehdbs8747@naver.com>
2022-09-06 17:22:47 +0900

    Add DP/boj_2294.py

---
+++ DP/boj_2294.py
@@ -0,0 +1,18 @@
+import sys
+
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+coins = [int(input()) for _ in range(n)]
+
+dp = [10001] * (k + 1)
+dp[0] = 0
+
+for coin in coins:
+    for i in range(coin, k + 1):
+        dp[i] = min(dp[i], dp[i - coin] + 1)
+
+if dp[k] == 10001:
+    print(-1)
+else:
+    print(dp[k])
