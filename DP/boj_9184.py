394ace9ea9cf0c0a4547024e7af8c0d2ae63015b
ehdbs0903 <ehdbs8747@naver.com>
2022-09-10 18:19:31 +0900

    Add DP/boj_9184.py

---
+++ DP/boj_9184.py
@@ -0,0 +1,24 @@
+import sys
+
+input = sys.stdin.readline
+
+dp = [[[0] * 21 for _ in range(21)] for _ in range(21)]
+
+def w(a, b, c):
+    if a <= 0 or b <= 0 or c <= 0:
+        return 1
+    if a > 20 or b > 20 or c > 20:
+        return w(20, 20, 20)
+    if dp[a][b][c]:
+        return dp[a][b][c]
+    if a < b < c:
+        dp[a][b][c] = w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c)
+        return w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c)
+    dp[a][b][c] = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1)
+    return w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1)
+
+while True:
+    a, b, c = map(int, input().split())
+    if a == b == c == -1:
+        break
+    print("w({0}, {1}, {2}) = {3}".format(a, b, c, w(a, b, c)))
