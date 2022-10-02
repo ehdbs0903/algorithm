5357500999e1a659e64c2f9efeeb75bbd15537b0
ehdbs0903 <ehdbs8747@naver.com>
2022-10-02 19:23:47 +0900

    Add PriorityQueue/boj_11279.py

---
+++ PriorityQueue/boj_11279.py
@@ -0,0 +1,13 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+
+heap = []
+for _ in range(int(input())):
+    x = int(input())
+    if x:
+        heapq.heappush(heap, -x)
+    else:
+        print(-heapq.heappop(heap) if heap else 0)
+
