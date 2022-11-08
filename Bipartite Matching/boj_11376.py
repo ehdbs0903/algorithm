7ecef3b34084a37a38fa5f0c949d6cb388c49a91
ehdbs0903 <ehdbs8747@naver.com>
2022-11-08 18:54:01 +0900

    Add Bipartite Matching/boj_11376.py

---
+++ Bipartite Matching/boj_11376.py
@@ -0,0 +1,31 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def dfs(v):
+    for g in graph[v]:
+        if not visited[g]:
+            visited[g] = 1
+
+            if not match[g] or dfs(match[g]):
+                match[g] = v
+                return True
+
+    return False
+
+
+n, m = map(int, input().split())
+graph = [[]] + [list(map(int, input().split()))[1:] for _ in range(n)]
+
+match = [0] * (m + 1)
+ans = 0
+
+for _ in range(2):
+    for i in range(1, n + 1):
+        visited = [0] * (m + 1)
+
+        if dfs(i):
+            ans += 1
+
+print(ans)
