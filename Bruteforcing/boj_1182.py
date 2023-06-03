57fd3933e174e25dbb8333d2470b55434a99a77d
ehdbs0903 <ehdbs8747@naver.com>
2023-06-03 15:22:52 +0900

    Add boj_1182.py

---
+++ Bruteforcing/boj_1182.py
@@ -0,0 +1,15 @@
+from itertools import combinations
+
+n, s = map(int, input().split())
+arr = list(map(int, input().split()))
+
+cnt = 0
+for i in range(1, n+1):
+    comb = combinations(arr, i)
+
+    for x in comb:
+        if sum(x) == s:
+            cnt += 1
+
+print(cnt)
+
