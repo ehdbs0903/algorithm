eca6ebfdaa375ed5c82556f5199f830df194bdaf
ehdbs0903 <ehdbs8747@naver.com>
2023-06-13 02:21:01 +0900

    feat : boj_11778.py

---
+++ Divide and Conquer/boj_11778.py
@@ -0,0 +1,37 @@
+def square(A, n):
+    if n == 1:
+        return A
+    else:
+        temp = square(A, n//2)
+        ret = multi_matrix(temp, temp)
+        if n % 2:
+            return multi_matrix(ret, A)
+        else:
+            return ret
+
+
+def multi_matrix(A, B):
+    C = [[0] * 2 for _ in range(2)]
+    for i in range(2):
+        for j in range(2):
+            for k in range(2):
+                C[i][j] += A[i][k] * B[k][j]
+            C[i][j] %= 1000000007
+    return C
+
+
+def gcd(a, b):
+    while a:
+        b %= a
+        a, b = b, a
+    return b
+
+
+n, m = map(int, input().split())
+
+A = [[1, 1], [1, 0]]
+
+gcd_nm = gcd(n, m)
+ans = square(A, gcd_nm)[0][1]
+
+print(ans)
