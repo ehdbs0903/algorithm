c3235ab92c02c13f322a60de8b5c94d1f19b8a5d
ehdbs0903 <82309982+ehdbs0903@users.noreply.github.com>
2024-04-04 22:59:39 +0900

    feat: add boj_11758.py

---
+++ Mathematics/boj_11758.py
@@ -0,0 +1,14 @@
+def ccw(p1,p2,p3):
+    return (p1[0]*p2[1] + p2[0]*p3[1] + p3[0]*p1[1] - (p2[0]*p1[1] + p3[0]*p2[1] + p1[0]*p3[1]))
+
+
+box = [list(map(int, input().split())) for _ in range(3)]
+
+ans = ccw(box[0],box[1],box[2])
+
+if ans > 0:
+    print(1)
+elif ans == 0:
+    print(0)
+else:
+    print(-1)
