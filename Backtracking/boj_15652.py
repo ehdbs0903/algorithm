d819ca3957812060665a4b2e73a2f674e364d888
ehdbs0903 <82309982+ehdbs0903@users.noreply.github.com>
2024-04-16 15:34:30 +0900

    feat: add boj_15652.py

---
+++ Backtracking/boj_15652.py
@@ -0,0 +1,19 @@
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+arr = [0]
+
+def backtracking():
+    if len(arr) == m + 1:
+        print(' '.join(map(str, arr[1:])))
+    else:
+        for i in range(1, n + 1):
+            if i >= arr[-1]:
+                arr.append(i)
+                backtracking()
+                arr.pop()
+
+backtracking()
