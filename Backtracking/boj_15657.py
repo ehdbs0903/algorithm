1f0605939adfafe9fa3680fa2923cc7f5b2d7b4b
ehdbs0903 <82309982+ehdbs0903@users.noreply.github.com>
2024-04-30 18:33:59 +0900

    feat: add boj_15657.py

---
+++ Backtracking/boj_15657.py
@@ -0,0 +1,17 @@
+n, m = map(int, input().split())
+nums = list(map(int, input().split()))
+
+nums.sort()
+arr = []
+
+def backtracking(idx):
+    if len(arr) == m:
+        print(*arr)
+    else:
+        for num in nums:
+            if len(arr) == 0 or num >= arr[-1]:
+                arr.append(num)
+                backtracking(idx + 1)
+                arr.pop()
+
+backtracking(0)
