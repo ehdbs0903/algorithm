ac678d245c760e27c5e635b09f2b11d5d858916d
ehdbs0903 <82309982+ehdbs0903@users.noreply.github.com>
2024-06-11 17:43:33 +0900

    feat:add boj_7598.py

---
+++ Bruteforcing/boj_7568.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+ 
+data = []
+ans = []
+for i in range(n):
+    a, b = map(int, input().split())
+    data.append((a, b))
+ 
+for i in range(n):
+    count = 0
+    for j in range(n):
+        if data[i][0] < data[j][0] and data[i][1] < data[j][1]:
+            count += 1
+    ans.append(count+1)
+
+print(*ans)
