011f6da32796669f81d81177b6b940495a37296c
ehdbs0903 <82309982+ehdbs0903@users.noreply.github.com>
2024-06-27 16:52:52 +0900

    feat: add boj_10814.cpp

--- Sorting/boj_10814.cpp
+++ Sorting/boj_10814.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
+bool compare(const pair<int, string>& a, const pair<int, string>& b) {
+	return a.first < b.first;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n;
+
+	cin >> n;
+
+	vector<pair<int, string>> arr;
+
+	int age;
+	string name;
+
+	while (n--) {
+		cin >> age >> name;
+		arr.emplace_back(age, name);
+	}
+
+	stable_sort(arr.begin(), arr.end(), compare);
+
+	for (auto it : arr)
+		cout << it.first << " " << it.second << "\n";
+
+	return 0;
+}
