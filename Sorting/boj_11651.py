2c78c144cf93933abe4767eba632a3b006f1d807
ehdbs0903 <82309982+ehdbs0903@users.noreply.github.com>
2024-06-30 23:50:55 +0900

    feat: add boj_11651.py

---
+++ Sorting/boj_11651.py
@@ -0,0 +1,12 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+points = [list(map(int, input().split())) for _ in range(n)]
+
+points.sort(key=lambda x: (x[1], x[0]))
+
+for p in points:
+    print(*p)
