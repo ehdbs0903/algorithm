666577d4f88d11e21c73a92ae14ec20b28361321
Doyun Kim <82309982+ehdbs0903@users.noreply.github.com>
2024-08-30 15:56:15 +0900

    feat: add boj_11399.cpp

---
+++ Greedy/boj_11399.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	vector<int> arr(n);
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+
+	sort(arr.begin(), arr.end());
+	
+	int sum = 0;
+	for (int i = 0; i < n; i++) {
+		sum += arr[i] * (n - i);
+	}
+
+	cout << sum;
+
+	return 0;
+}
