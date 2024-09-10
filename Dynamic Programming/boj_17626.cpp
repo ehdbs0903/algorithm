a921a34760c47ec8d077794874ba5d877131acca
Doyun Kim <82309982+ehdbs0903@users.noreply.github.com>
2024-09-11 00:10:31 +0900

    feat: add boj_17626.cpp

---
+++ Dynamic Programming/boj_17626.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    
+    cin >> n;
+
+    vector<int> dp(n+1);
+
+    for (int i = 1; i <= n; i++) {
+        dp[i] = i;
+        for (int j = 1; j * j <= i; j++) {
+            dp[i] = min(dp[i], dp[i - j * j] + 1);
+        }
+    }
+
+    cout << dp[n];
+
+    return 0;
+}
